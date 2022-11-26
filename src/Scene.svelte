9328be6e6f7c442ac142d1d89d21a89cb99ed655
Shiyun <lushiyun@outlook.com>
2022-11-26 17:18:18 -0500

    Clean up

--- src/Scene.svelte
+++ src/Scene.svelte
@@ -1,48 +1,27 @@
 <script>
-	import { OrbitControls, Three, useFrame, useTexture } from '@threlte/core';
+	import { OrbitControls, Three } from '@threlte/core';
 	import {
-		BufferAttribute,
-		Color,
+		DoubleSide,
 		Mesh,
 		PerspectiveCamera,
 		PlaneGeometry,
 		ShaderMaterial,
-		Vector2,
 	} from 'three';
 	import fragmentShader from './shaders/fragment';
 	import vertexShader from './shaders/vertex';
-
-	const texture = useTexture('./flag-french.jpg');
 
 	const material = new ShaderMaterial({
 		vertexShader: vertexShader,
 		fragmentShader: fragmentShader,
 		transparent: true,
-		uniforms: {
-			uFrequency: { value: new Vector2(10, 5) },
-			uTime: { value: 0 },
-			uColor: { value: new Color('orange') },
-			uTexture: { value: texture },
-		},
+		side: DoubleSide,
 	});
 
 	const geometry = new PlaneGeometry(1, 1, 32, 32);
-
-	const count = geometry.attributes.position.count;
-	const randoms = new Float32Array(count);
-	for (let i = 0; i < count; i++) {
-		randoms[i] = Math.random();
-	}
-
-	geometry.setAttribute('aRandom', new BufferAttribute(randoms, 1));
-
-	useFrame(({ clock }) => {
-		material.uniforms.uTime.value = clock.getElapsedTime();
-	});
 </script>
 
 <Three type={PerspectiveCamera} makeDefault position={[10, 10, 10]} fov={24}>
 	<OrbitControls />
 </Three>
 
-<Three type={Mesh} {geometry} {material} scale.y={2 / 3} />
+<Three type={Mesh} {geometry} {material} />
--- src/shaders/vertex.js
+++ src/shaders/vertex.js
@@ -1,28 +1,10 @@
 const vertexShader = `
-	uniform vec2 uFrequency;
-	uniform float uTime;
-
-	varying vec2 vUv;
-	varying float vElevation;
-
 	void main() {
 		vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-
-		float elevation = sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
-		elevation += sin(modelPosition.y * uFrequency.y + uTime) *0.1;
-
-		modelPosition.z += elevation;
-
-		modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
-		modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
-
 		vec4 viewPosition = viewMatrix * modelPosition;
 		vec4 projectionPosition = projectionMatrix * viewPosition;
 
 		gl_Position = projectionPosition;
-
-		vUv = uv;
-		vElevation = elevation;
 	}
 `;
 
